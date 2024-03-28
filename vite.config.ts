f1f0c5c86cb75512ae55a53c2fbad38fd4b391ac
Lukas Berbuer <lukas.berbuer@gmail.com>
2024-03-28 14:04:42 +0100

    fix: set base url

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://openae-io.github.io/features-playground/",
   plugins: [vue()],
   resolve: {
     alias: {
