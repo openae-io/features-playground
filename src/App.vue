0356c8d7cbfc39666fdc11ef6789d02a076c900a
Lukas Berbuer <lukas.berbuer@gmail.com>
2024-04-03 14:03:41 +0200

    feat: open & share code links

--- src/App.vue
+++ src/App.vue
@@ -8,9 +8,10 @@
         OpenAE <span class="font-weight-light">Features Playground</span>
       </v-toolbar-title>
       <template #append>
+        <v-btn prepend-icon="mdi-share-variant" @click="shareLink">Share</v-btn>
         <v-menu>
           <template #activator="{ props }">
-            <v-btn prepend-icon="mdi-launch" variant="text" v-bind="props" />
+            <v-btn prepend-icon="mdi-launch" v-bind="props">Links</v-btn>
           </template>
           <v-list density="compact">
             <v-list-item
@@ -33,7 +34,7 @@
         <pane min-size="10">
           <Suspense>
             <Analysis :code="code" />
-            <template #fallback> Loading... </template>
+            <template #fallback>Loading...</template>
           </Suspense>
         </pane>
       </splitpanes>
@@ -43,6 +44,7 @@
 
 <script setup lang="ts">
 import { ref } from "vue";
+import { useShare } from "@vueuse/core";
 import { Splitpanes, Pane } from "splitpanes";
 import "splitpanes/dist/splitpanes.css";
 import CodeEditor from "./components/CodeEditor.vue";
@@ -53,14 +55,28 @@ const links = [
   { title: "GitHub", icon: "mdi-github", href: "https://github.com/openae-io" },
 ];
 
+const urlParams = new URLSearchParams(window.location.search);
+
 const codeExample = `
 import numpy as np
 
 def rms(signal: np.ndarray) -> float:
     return np.sqrt(np.mean(signal ** 2))
 `;
+const code = ref(
+  urlParams.has("code") ? atob(urlParams.get("code") ?? "") : codeExample.trimStart(),
+);
 
-const code = ref(codeExample.trimStart());
+const { share } = useShare();
+function shareLink() {
+  const url = new URL(location.href);
+  url.searchParams.set("code", btoa(code.value));
+  share({
+    title: "OpenAE Features Playground",
+    text: "Code snippet",
+    url: url.href,
+  });
+}
 </script>
 
 <style scoped>
